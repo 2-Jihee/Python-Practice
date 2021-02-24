1868e909a84912b162958f1c928cd8bce2e094a9
jihee_lee <jihee0825@gmail.com>
2021-02-24 23:34:38 +0900

    기본값

--- Practice.py
+++ Practice.py
@@ -1,26 +1,14 @@
-def open_account() :
-    print("새로운 계좌가 생성되었습니다.")
+# # 기본값
+def profile(name, age, main_lang):
+    print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
 
-def deposit(balance, money) :  # 입금
-    print("입금이 완료되었습니다. 잔액은 {} 원 입니다.".format(balance + money))
-    return balance + money
+profile("이지희", 28, "파이썬")
+profile("채승주", 28, "자바")
 
-def withraw(balance, money) : # 출금
-    if balance >= money : # 잔액이 출금액보다 많으면
-        print("출금이 완료되었습니다. 잔액은 {} 원 입니다.".format(balance - money))
-        return balance - money
-    else :
-        print("출금이 완료되지 않았습니다. 잔액은 {} 원 입니다.".format(balance))
-        return balance
 
-def withraw_night(balance, money) : # 저녁에 출금
-    commision = 100 # 수수료 100원
-    return commision, balance - money - commision
+# 같은 학교, 같은 학년, 같은 수업
+def profile(name, age = 19, main_lang = "파이썬"):
+    print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
 
-# 입금하기, 출금하기
-balance = 0
-balance = deposit(balance, 1000)
-# balance = withraw(balance, 2000)
-# balance = withraw(balance, 500)
-commision, balance = withraw_night(balance, 500)
-print("수수료는 {0} 원이며, 잔액은 {1} 원입니다.".format(commision, balance))
+profile("이지희")
+profile("채승주")
