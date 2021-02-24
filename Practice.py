633dab23726199f211a18f83362d256a13873e93
jihee_lee <jihee0825@gmail.com>
2021-02-24 23:36:41 +0900

    기본값 3

--- Practice.py
+++ Practice.py
@@ -1,9 +1,9 @@
 # 기본값
-def profile(name, age, main_lang):
-    print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
+# def profile(name, age, main_lang):
+#     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
 
-profile("이지희", 28, "파이썬")
-profile("채승주", 28, "자바")
+# profile("이지희", 28, "파이썬")
+# profile("채승주", 28, "자바")
 
 
 # 같은 학교, 같은 학년, 같은 수업
