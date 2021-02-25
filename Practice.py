8a75a4bce4031e4e8b52a773ef7ede20640cde43
jihee_lee <jihee0825@gmail.com>
2021-02-25 23:15:47 +0900

    키워드값

--- Practice.py
+++ Practice.py
@@ -1,14 +1,6 @@
-# 기본값
-# def profile(name, age, main_lang):
-#     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
+# 키워드 값
+def profile(name, age, main_lang) :
+    print(name, age, main_lang)
 
-# profile("이지희", 28, "파이썬")
-# profile("채승주", 28, "자바")
-
-
-# 같은 학교, 같은 학년, 같은 수업
-def profile(name, age = 19, main_lang = "파이썬"):
-    print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
-
-profile("이지희")
-profile("채승주")
+profile(name = "유재석", main_lang = "파이썬", age = 20)
+profile(main_lang = "자바",  age = 25, name = "김태호")
