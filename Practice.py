8bec1687041776082e54acd87441688ac309ea5e
jihee_lee <jihee0825@gmail.com>
2021-03-01 23:57:08 +0900

    import sys

--- Practice.py
+++ Practice.py
@@ -1,5 +1,9 @@
 # 표준 입출력
-print("Python" + "Java")
-print("Python", "Java", sep = ",", end = "?")
-print("무엇이 더 재밌을까여?")
-print("Python", "Java", "JavaScript", sep = " vs ")
+# print("Python" + "Java")
+# print("Python", "Java", sep = ",", end = "?")
+# print("무엇이 더 재밌을까여?")
+# print("Python", "Java", "JavaScript", sep = " vs ")
+
+import sys
+print("Python", "Java", file = sys.stdout) # 표준 입력 처리
+print("Python", "Java", file = sys.stderr) # 표준 에러 처리
