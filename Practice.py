ba5ea51b256adf1788a21b9ee2c403602a13b8b8
jihee_lee <jihee0825@gmail.com>
2021-03-01 23:53:57 +0900

    + end=

--- Practice.py
+++ Practice.py
@@ -1,4 +1,5 @@
 # 표준 입출력
 print("Python" + "Java")
-print("Python", "Java", sep = ",")
+print("Python", "Java", sep = ",", end = "?")
+print("무엇이 더 재밌을까여?")
 print("Python", "Java", "JavaScript", sep = " vs ")
