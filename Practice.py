38238ffe2640f7c0d5f6acb60b72bf29d9749116
jihee_lee <jihee0825@gmail.com>
2021-03-02 23:22:01 +0900

    input 문자열 입력

--- Practice.py
+++ Practice.py
@@ -1,20 +1,23 @@
 # 표준 입출력
-# print("Python" + "Java")
-# print("Python", "Java", sep = ",", end = "?")
-# print("무엇이 더 재밌을까여?")
-# print("Python", "Java", "JavaScript", sep = " vs ")
+print("Python" + "Java")
+print("Python", "Java", sep = ",", end = "?")
+print("무엇이 더 재밌을까여?")
+print("Python", "Java", "JavaScript", sep = " vs ")
 
-# import sys
-# print("Python", "Java", file = sys.stdout) # 표준 입력 처리
-# print("Python", "Java", file = sys.stderr) # 표준 에러 처리
+import sys
+print("Python", "Java", file = sys.stdout) # 표준 입력 처리
+print("Python", "Java", file = sys.stderr) # 표준 에러 처리
 
-# # 시험 성적
-# scores = {"수학" : 0, "영어" : 50, "코딩" : 100 }
-# for subject, score in scores.items() :
-#     # print(subject, score)
-#     print(subject.ljust(4), str(score).rjust(4), sep = " : ")
+# 시험 성적
+scores = {"수학" : 0, "영어" : 50, "코딩" : 100 }
+for subject, score in scores.items() :
+    # print(subject, score)
+    print(subject.ljust(4), str(score).rjust(4), sep = " : ")
 
 # 은행 대기순번표
 # 001, 002, 003, ...
 for num in range(1, 21) :
     print("대기번호 : " + str(num).zfill(3))
+
+answer = input("아무값이나 입력하세요 : ")
+print("입력하신 값은" + answer + "입니다.")
