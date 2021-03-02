0ca2d8247bda7d4d5d9c7a9e0b6af4272a73f662
jihee_lee <jihee0825@gmail.com>
2021-03-02 23:15:27 +0900

    + zfill()

--- Practice.py
+++ Practice.py
@@ -8,8 +8,13 @@
 # print("Python", "Java", file = sys.stdout) # 표준 입력 처리
 # print("Python", "Java", file = sys.stderr) # 표준 에러 처리
 
-# 시험 성적
-scores = {"수학" : 0, "영어" : 50, "코딩" : 100 }
-for subject, score in scores.items() :
-    # print(subject, score)
-    print(subject.ljust(4), str(score).rjust(4), sep = " : ")
+# # 시험 성적
+# scores = {"수학" : 0, "영어" : 50, "코딩" : 100 }
+# for subject, score in scores.items() :
+#     # print(subject, score)
+#     print(subject.ljust(4), str(score).rjust(4), sep = " : ")
+
+# 은행 대기순번표
+# 001, 002, 003, ...
+for num in range(1, 21) :
+    print("대기번호 : " + str(num).zfill(3))
