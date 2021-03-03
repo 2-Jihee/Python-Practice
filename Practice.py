0325e4c052f39ac59c741f730084775fbdbd0aab
jihee_lee <jihee0825@gmail.com>
2021-03-03 23:27:04 +0900

    다양한 출력 포맷

--- Practice.py
+++ Practice.py
@@ -1,23 +1,24 @@
-# 표준 입출력
-print("Python" + "Java")
-print("Python", "Java", sep = ",", end = "?")
-print("무엇이 더 재밌을까여?")
-print("Python", "Java", "JavaScript", sep = " vs ")
+# 다양한 출력 포맷
+# 빈 자리는 빈 공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+print("{0: >10}".format(500))
 
-import sys
-print("Python", "Java", file = sys.stdout) # 표준 입력 처리
-print("Python", "Java", file = sys.stderr) # 표준 에러 처리
+# 양수일 땐 +로 표시, 음수일 땐 -로 표시
+print("{0: >+10}".format(500))
+print("{0: >+10}".format(-500))
 
-# 시험 성적
-scores = {"수학" : 0, "영어" : 50, "코딩" : 100 }
-for subject, score in scores.items() :
-    # print(subject, score)
-    print(subject.ljust(4), str(score).rjust(4), sep = " : ")
+# 왼쪽 정렬하고, 빈칸을 _로 채움
+print("{0:_<10}".format(500))
+print("{0:_<+10}".format(500))
 
-# 은행 대기순번표
-# 001, 002, 003, ...
-for num in range(1, 21) :
-    print("대기번호 : " + str(num).zfill(3))
+# 3자리마다 ,(콤마)를 찍어주기
+print("{0:,}".format(100000000000000))
+# 3자리마다 ,(콤마)를 찍어주기, + - 부호도 붙이기
+print("{0:+,}".format(100000000000000))
+print("{0:+,}".format(-100000000000000))
+# 3자리마다 ,(콤마)를 찍어주기, + - 부호도 붙이고, 자릿수 확보하고, 빈 자리는 ^로 채워주기
+print("{0:^<+30,}".format(100000000000000))
 
-answer = input("아무값이나 입력하세요 : ")
-print("입력하신 값은" + answer + "입니다.")
+# 소수점 출력
+print("{0:f}".format(5/3))
+# 소수점 특정 자리수까지만 표시 (소수점 3째 자리에서 반올림)
+print("{0:.2f}".format(5/3))
