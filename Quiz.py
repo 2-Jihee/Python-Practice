155d5563a4b242e52239dc51ca0cda0b3af05ba6
jihee_lee <jihee0825@gmail.com>
2021-03-07 22:55:02 +0900

    선생님 풀이

--- Quiz.py
+++ Quiz.py
@@ -6,13 +6,22 @@
 이름 :
 업무 요약 :
 
-1주차부터 50주차까지의 보고서 파일을 만드는 프로그램으 작성하시오.
+1주차부터 10주차까지의 보고서 파일을 만드는 프로그램으 작성하시오.
 
 조건 : 파일명은 '1주차.txt', '2주차 txt', ...와 같이 만듭니다. '''
 
-report_file = open("{0}주차.txt".format(1), "w", encoding="utf8")
-print("- {0} 주차 주간 보고 -".format(1), file=report_file)
-print("부서 : ", file=report_file)
-print("이름 : ", file=report_file)
-print("업무 요약 : ", file=report_file)
-report_file.close()
+# 내 코드ㅠㅠ(못품)
+# report_file = open("{0}주차.txt".format(1), "w", encoding="utf8")
+# print("- {0} 주차 주간 보고 -".format(1), file=report_file)
+# print("부서 : ", file=report_file)
+# print("이름 : ", file=report_file)
+# print("업무 요약 : ", file=report_file)
+# report_file.close()
+
+# 선생님 풀이
+for i in range(1, 11) :
+    with open(str(i) + "주차.txt", "w", encoding="utf8") as report_file :
+        report_file.write("- {0} 주차 주간 보고 -".format(i))
+        report_file.write("\n부서 :")
+        report_file.write("\n이름 :")
+        report_file.write("\n업무 요약 :")
