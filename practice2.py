cf645ca66e0766bbba58bb440749a3f2da5c2810
jihee_lee <jihee0825@gmail.com>
2021-03-09 23:23:12 +0900

    멤버 변수 1

--- practice2.py
+++ practice2.py
@@ -7,6 +7,17 @@ class Unit :
         print("{0} 유닛이 생성 되었습니다.".format(self.name))
         print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
 
-marine1 = Unit("마린", 40, 5)
-marine2 = Unit("마린", 40, 5)
-tank = Unit("탱크", 150, 35)
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 35)
+
+# 레이스 : 공중 유닛, 비행기. 클로킹 (상대방에게 보이지 않음)
+wrauth1 = Unit("레이스", 80, 5)
+print("유닛 이름 : {0}, 공격력 : {1}".format(wrauth1.name, wrauth1.damage))
+
+# 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+wrauth2 = Unit("레이스", 80, 5)
+wrauth2.clocking = True
+
+if wrauth2.clocking == True :
+    print("{0}는 현재 클로킹 상태입니다.".format(wrauth2.name))
