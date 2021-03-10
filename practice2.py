710e62d7b4223ff452a89ebeff8d71d62febb41b
jihee_lee <jihee0825@gmail.com>
2021-03-10 20:42:28 +0900

    메소드

--- practice2.py
+++ practice2.py
@@ -7,17 +7,28 @@ class Unit :
         print("{0} 유닛이 생성 되었습니다.".format(self.name))
         print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
 
-# marine1 = Unit("마린", 40, 5)
-# marine2 = Unit("마린", 40, 5)
-# tank = Unit("탱크", 150, 35)
 
-# 레이스 : 공중 유닛, 비행기. 클로킹 (상대방에게 보이지 않음)
-wrauth1 = Unit("레이스", 80, 5)
-print("유닛 이름 : {0}, 공격력 : {1}".format(wrauth1.name, wrauth1.damage))
+# 공격 유닛
+class AttackUnit :
+    def __init__(self, name, hp, damage) :
+        self.name = name
+        self.hp = hp
+        self.damage = damage
 
-# 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
-wrauth2 = Unit("레이스", 80, 5)
-wrauth2.clocking = True
+    def attack(self, location) :
+        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 : {2}]"\
+            .format(self.name, location, self.damage))
+    def damaged(self, damage) :
+        print("{0} : {1}데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1}입니다.".format(self.name, self.hp))
+        if self.hp <= 0 :
+            print("{0} : 파괴되었습니다.".format(self.name))
 
-if wrauth2.clocking == True :
-    print("{0}는 현재 클로킹 상태입니다.".format(wrauth2.name))
+# 파이어뱃 : 공격유닛, 화염방사기
+firebat1 = AttackUnit("파이어뱃", 50, 16)
+firebat1.attack("5시")
+
+# 공격 2번 받는다고 가정.
+firebat1.damaged(25)
+firebat1.damaged(25)
