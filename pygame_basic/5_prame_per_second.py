bf0a85c91886a657315fa54edc427172ffa0f11c
jihee_lee <jihee0825@gmail.com>
2021-03-22 16:08:04 +0900

    FPS

--- pygame_basic/5_prame_per_second.py
+++ pygame_basic/5_prame_per_second.py
@@ -10,6 +10,9 @@ screen = pygame.display.set_mode((screen_width, screen_hight))
 # 화면 타이틀 설정
 pygame.display.set_caption("Jihee Game")  # 게임이름
 
+# FPS
+clock = pygame.time.Clock()
+
 # 배경 이미지 불러오기
 background = pygame.image.load("C:/Users/USER/Desktop/Git-PythonWorkspace/pygame_basic/background.png")
 
@@ -25,22 +28,27 @@ character_y_pos = screen_hight - character_hight # 화면 세로 크기 가장 �
 to_x = 0
 to_y = 0
 
+# 이동 속도
+character_speed = 0.6
+
 # 이벤트 루프
 running = True  # 게임이 진행중인가?
 while running:
+    dt = clock.tick(10)  # 게임화면의 초당 프레임 수를 설정
+
     for event in pygame.event.get():  # 어떤 이벤트가 발생하였는가?
         if event.type == pygame.QUIT:  # 창이 닫히는 이벤트가 발생하였는가?
             running = False  # 게임이 진행중이 아님
 
         if event.type == pygame.KEYDOWN:  # 키가 눌러졌는지 확인
             if event.key == pygame.K_LEFT:  # 캐릭터를 왼쪽으로
-                to_x -= 5  # to_x = to_x - 5
+                to_x -= character_speed  # to_x = to_x - 5
             elif event.key == pygame.K_RIGHT:  # 캐릭터를 오른쪽으로
-                to_x += 5
+                to_x += character_speed
             elif event.key == pygame.K_UP:  # 캐릭터를 위로
-                to_y -= 5
+                to_y -= character_speed
             elif event.key == pygame.K_DOWN:  # 캐릭터를 아래로
-                to_y += 5
+                to_y += character_speed
 
         if event.type == pygame.KEYUP:  # 방향키를 떼면 멈춤
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -48,8 +56,8 @@ while running:
             elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 to_y = 0
 
-    character_x_pos += to_x
-    character_y_pos += to_y
+    character_x_pos += to_x * dt
+    character_y_pos += to_y * dt
 
     # 가로 경계값 처리
     if character_x_pos < 0:
