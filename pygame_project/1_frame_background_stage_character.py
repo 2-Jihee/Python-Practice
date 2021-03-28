9a618a1f77460e6148b6d57e592dffea06e1bce1
jihee_lee <jihee0825@gmail.com>
2021-03-28 23:31:02 +0900

    배경 만들기

---
+++ pygame_project/1_frame_background_stage_character.py
@@ -0,0 +1,46 @@
+import os
+import pygame
+############################################################################
+# 기본 초기화 (반드시 해야하는 것들)
+pygame.init()  # 초기화 (반드시 필요한 작업)
+
+# 화면 크기 설정
+screen_width = 640  # 가로 크기
+screen_height = 480  # 세로 크기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("Jihee Pang")  # 게임이름
+
+# FPS
+clock = pygame.time.Clock()
+############################################################################
+
+# 1. 사용자 게임 초기화 (배경화면, 게임 이미지, 좌표, 속도, 폰트 둥)
+current_path = os.path.dirname(__file__)  # 현재 파일의 위치 반환
+image_path = os.path.join(current_path, "images")  # images 폴더 위치 반환
+
+# 배경 만들기
+background = pygame.image.load(os.path.join(image_path, "background"))
+
+
+running = True
+while running:
+    dt = clock.tick(30)
+
+    # 2. 이벤트 처리 (키보드, 마우스 등)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # 3. 게임 캐릭터 위치 정의
+    
+    # 4. 충돌 처리
+   
+    # 5. 화면에 그리기
+    screen.blit(background, (0,0))
+
+
+    pygame.display.update()
+
+pygame.quit()
