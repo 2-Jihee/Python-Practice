dccfe5734264dd548daf9af3abaab1ef0b2c0015
jihee_lee <jihee0825@gmail.com>
2021-03-29 23:13:29 +0900

    캐릭터 그리기

--- pygame_project/1_frame_background_stage_character.py
+++ pygame_project/1_frame_background_stage_character.py
@@ -28,6 +28,14 @@ stage = pygame.image.load(os.path.join(image_path, "stage.png"))
 stage_size = stage.get_rect().size
 stage_height = stage_size[1]  # 스테이지의 높이 위에 캐릭터를 두기 위해 사용
 
+# 캐릭터 만들기
+character = pygame.image.load(os.path.join(image_path, "character.png"))
+character_size = character.get_rect().size
+character_width =  character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) - (character_width / 2)
+character_y_pos = screen_height - character_height - stage_height
+
 running = True
 while running:
     dt = clock.tick(30)
@@ -44,6 +52,7 @@ while running:
     # 5. 화면에 그리기
     screen.blit(background, (0,0))
     screen.blit(stage, (0, screen_height - stage_height))
+    screen.blit(character, (character_x_pos, character_y_pos))
 
     pygame.display.update()
 
