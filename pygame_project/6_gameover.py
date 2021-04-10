e6aed8056bb0581c26825a4513e803e69265696b
jihee_lee <jihee0825@gmail.com>
2021-04-10 23:29:41 +0900

    오류수정

--- pygame_project/6_gameover.py
+++ pygame_project/6_gameover.py
@@ -225,6 +225,11 @@ while running:
                         "init_spd_y" : ball_speed_y[ball_img_idx + 1]})
                 break
 
+        else: # 계속 게임을 진행
+            continue # 안쪽 for문 조건이 맞지 않으면 continue. 바깥 for문 계속 수행
+        break 
+
+             
     # 충돌된 공 or 무기 없애기
     if ball_to_remove > -1:
         del balls[ball_to_remove]
